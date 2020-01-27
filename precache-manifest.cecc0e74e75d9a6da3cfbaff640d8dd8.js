7f07e91a735f1fea01b8930e59e53878b5068165
Roman <Roman.Vengl1@gmail.com>
2020-01-27 10:57:45 +0200

    Updates

---
+++ precache-manifest.cecc0e74e75d9a6da3cfbaff640d8dd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442d40e3e43193463db36496ac9b4556",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "1dbd2a2191787634ff25",
+    "url": "/goit-react-hw-02-feedback/static/js/2.0e13d6bf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-feedback/static/js/2.0e13d6bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e222440df9ec116293d5",
+    "url": "/goit-react-hw-02-feedback/static/js/main.f0d8d06f.chunk.js"
+  },
+  {
+    "revision": "123d81bea882563839a7",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.d87da01c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-feedback/precache-manifest.5105ec46be83e0635909ce16f16ebf9f.js"
+  "/goit-react-hw-02-feedback/precache-manifest.cecc0e74e75d9a6da3cfbaff640d8dd8.js"
 );
 
 self.addEventListener('message', (event) => {
